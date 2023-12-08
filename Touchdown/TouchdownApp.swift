a7142e413f306152aba84ddbeb493ea0f764967a
aderemilekun <adelefatai@yahoo.com>
2023-12-08 16:16:58 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Aderemilekun Adele on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
