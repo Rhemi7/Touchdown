5e9a5b48d5ede4d1d2d1943c12a3691142c1d930
aderemilekun <adelefatai@yahoo.com>
2023-12-12 13:35:19 +0100

    Created logo view

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,40 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Aderemilekun Adele on 12/12/2023.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 3) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 14, height: 14, alignment: .center)
+                
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            
+        } //: HSTACK
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
