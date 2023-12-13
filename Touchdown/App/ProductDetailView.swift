3b9ae809bd96c6984d5abe5d4000a840f0469c54
aderemilekun <adelefatai@yahoo.com>
2023-12-13 21:39:16 +0100

    Refactored app with Shop environment object

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -11,6 +11,8 @@ struct ProductDetailView: View {
     
     // MARK: - PROPERTIES
     
+    @EnvironmentObject var shop : Shop
+    
     // MARK: - BODY
     
     var body: some View {
@@ -39,7 +41,7 @@ struct ProductDetailView: View {
                 
                 // DESCRIPTION
                 ScrollView(.vertical, showsIndicators: false, content: {
-                    Text(sampleProduct.description)
+                    Text(shop.selectedProduct?.description ?? sampleProduct.description)
                         .font(.system(.body, design: .rounded))
                         .foregroundColor(.gray)
                         .multilineTextAlignment(.leading)
@@ -67,9 +69,9 @@ struct ProductDetailView: View {
         .ignoresSafeArea(.all, edges: .all)
         .background(
             Color(
-                red: sampleProduct.red,
-                green: sampleProduct.green,
-                blue: sampleProduct.blue
+                red: shop.selectedProduct?.red ?? sampleProduct.red,
+                green: shop.selectedProduct?.green ?? sampleProduct.green,
+                blue: shop.selectedProduct?.blue ?? sampleProduct.blue
             ).ignoresSafeArea(.all, edges: .all)
         )
     }
@@ -78,6 +80,7 @@ struct ProductDetailView: View {
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ProductDetailView()
+            .environmentObject(Shop())
             .previewLayout(.fixed(width: 375, height: 812))
     }
 }
--- Touchdown/View/Detail/HeaderDetailView.swift
+++ Touchdown/View/Detail/HeaderDetailView.swift
@@ -11,13 +11,15 @@ struct HeaderDetailView: View {
     
     // MARK: - PROPERTIES
     
+    @EnvironmentObject var shop: Shop
+    
     // MARK: - BODY
     
     var body: some View {
         VStack(alignment: .leading, spacing: 6) {
             Text("Protective Gear")
             
-            Text(sampleProduct.name)
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
                 .font(.largeTitle)
                 .fontWeight(.black)
         } //: VSTACK
@@ -28,6 +30,7 @@ struct HeaderDetailView: View {
 struct HeaderDetailView_Previews: PreviewProvider {
     static var previews: some View {
         HeaderDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
             .background(Color.gray)
